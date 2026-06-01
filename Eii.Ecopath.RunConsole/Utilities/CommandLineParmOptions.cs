fb354c71df5adbf1b37463a3269adb7b5e3cf37d
Rikkert01 <rik.kreeftenberg@spinsoft.nl>
2026-06-01 12:51:00 +0200

    +semver: major Added the correct packages

---
+++ Eii.Ecopath.RunConsole/Utilities/CommandLineParmOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+class CommandLineParmOptions
+{
+    [Option('i', "info", HelpText = "Run instructions file.", Required = true)]
+    public string? RunInfo { get; set; }
+
+    [Option('o', "output", HelpText = "Output folder location")]
+    public string? Output{ get; set; }
+
+    [Option('t', HelpText = "Show command tree")]
+    public bool ShowTree { get; set; } 
+
+    [Option('c', HelpText = "Show all commands")]
+    public bool ShowCommands { get; set; }
+
+}
